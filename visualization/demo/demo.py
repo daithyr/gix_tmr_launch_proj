c714ed98c0af4355111a49d508af647f5fb04e1b
Mingheng Wu <wmhst7@gmail.com>
2024-12-04 23:07:34 -0800

    feat: add visualization

--- visualization/demo/demo.py
+++ visualization/demo/demo.py
@@ -2,7 +2,8 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-equal_size = True
+equal_size = False
+use_background = False
 
 with open('../data/raw.json', 'r') as file:
     data = json.load(file)
@@ -16,10 +17,12 @@ if equal_size:
 else:
     sizes = [s * 200 for s in signal_strength]
 
-img = mpimg.imread('../data/map.jpg')
-
 plt.figure(figsize=(8, 6))
-plt.imshow(img, extent=[0, 100, 0, 100], origin='lower', aspect='auto')
+
+if use_background:
+    img = mpimg.imread('../data/map.jpg')
+    plt.imshow(img, extent=[0, 100, 0, 100], origin='lower', aspect='auto')
+
 scatter = plt.scatter(x, y, c=signal_strength, s=sizes, cmap='viridis', alpha=0.8)
 plt.colorbar(scatter, label="Signal Strength")
 plt.xlabel("X Coordinate")
