44d72c01c37b48ad6f4c4bf70c042e41c43d0311
Mingheng Wu <wmhst7@gmail.com>
2024-12-04 23:05:43 -0800

    feat: add visualization

--- visualization/demo/demo.py
+++ visualization/demo/demo.py
@@ -1,5 +1,6 @@
 import json
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 equal_size = True
 
@@ -15,11 +16,14 @@ if equal_size:
 else:
     sizes = [s * 200 for s in signal_strength]
 
+img = mpimg.imread('../data/map.jpg')
+
 plt.figure(figsize=(8, 6))
+plt.imshow(img, extent=[0, 100, 0, 100], origin='lower', aspect='auto')
 scatter = plt.scatter(x, y, c=signal_strength, s=sizes, cmap='viridis', alpha=0.8)
 plt.colorbar(scatter, label="Signal Strength")
 plt.xlabel("X Coordinate")
 plt.ylabel("Y Coordinate")
 plt.title("2D Signal Strength Visualization")
-plt.grid(True)
+plt.grid(True, alpha=0.5)
 plt.show()
